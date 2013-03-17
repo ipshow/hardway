1151947b358e1bb0bf4c12bc9ddf60b554103d54
ubuntu <ipshow@msn.com>
2013-03-17 17:29:06 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hardway::Application.config.secret_token = '6ade19bb84510d9471a644d57157065faf292e74f0d01bfc3569f16a928114d573ccef7c6c862eb419e70cdc3bc77d85ac9d8caa82636ef4c2eb3ccf3a0a5701'
